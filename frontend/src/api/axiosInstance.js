03151ba1e52475c48e8104e18b5f24beecf5f4f5
Aliaksandr-78 <sasha-mv21634@yandex.by>
2025-05-28 13:26:53 +0300

    deploy 13 client

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://hospital-backend-6ni9.onrender.com"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://hospital-backend-6ni9.onrender.com/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
